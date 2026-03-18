94ca056ce099173b8299fbc8e2dc16da5ebf8345
Dnyanesh K <dakanpurne@gmail.com>
2026-03-18 17:02:22 +0530

    test

--- aggrid/src/app/app.component.ts
+++ aggrid/src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'aggrid';
+
+  gridOptions = {
+    suppressRowTransform: true
+  };
+
+  rowData = [
+    { group: 'Row', value1: 'Row 1', value2: 'Row 1', span: 3 },
+    { group: '', value1: 'Row 2', value2: 'Row 2', span: 1 },
+    { group: '', value1: 'Row 3', value2: 'Row 3', span: 1 },
+
+    { group: 'Row', value1: 'Row 1', value2: 'Row 1', span: 3 },
+    { group: '', value1: 'Row 2', value2: 'Row 2', span: 1 },
+    { group: '', value1: 'Row 3', value2: 'Row 3', span: 1 }
+  ];
+
+  columnDefs: ColDef[] = [
+    {
+      headerName: 'Row',
+      field: 'group',
+      rowSpan: params => params.data.span,
+
+      // 🔥 IMPORTANT: apply class only to spanning cell
+      cellClassRules: {
+        'span-cell': params => params.data.span > 1,
+        'hidden-cell': params => params.data.span === 1
+      }
+    },
+    { field: 'value1' },
+    { field: 'value2' }
+  ];
+
 }
